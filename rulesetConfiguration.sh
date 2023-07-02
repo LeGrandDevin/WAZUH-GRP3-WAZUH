8c29effbd359c8d2365c324a3b43e0de6b1ab180
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2023-07-02 21:26:49 +0200

    Update rulesetConfiguration.sh

--- rulesetConfiguration.sh
+++ rulesetConfiguration.sh
@@ -1,3 +1,5 @@
 #Cp ossec.conf and local_rules.xml with our configurations to their directories
+read -p 'email: ' email
+python3 emailConf.py $email
 cp ossec.conf /var/ossec/etc/
 cp local_rules.xml /var/ossec/rules/
